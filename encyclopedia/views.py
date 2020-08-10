77affef383730067402e902c2e9464cabc99f19c
cgrs27 <cgrs27@gmail.com>
2020-08-10 16:55:35 -0400

    creando cambios en wiki/__pycache__/urls.cpython-38.pyc

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
 
+class MyForm(forms.Form):
+    q = forms.CharField()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def entry(request):
+    return render(request, "encyclopedia/entry.html", {})
+    pass
