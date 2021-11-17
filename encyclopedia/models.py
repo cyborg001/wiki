5d4602ef850c4aa1f8c948bb7d163a1a5235847e
Carlos Ramirez <cgrs27@gmail.com>
2021-11-17 18:04:08 -0400

    modificando models.py

--- encyclopedia/models.py
+++ encyclopedia/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractBaseUser):
+    pass
+
